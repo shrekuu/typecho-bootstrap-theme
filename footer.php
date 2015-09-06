defde029384cf6420fe76a3ed4e4ed3174593ed8
shrekuu <shrekuu@gmail.com>
2015-09-07 00:23:55 +0800

    rm some irrelevant link

--- footer.php
+++ footer.php
@@ -9,7 +9,7 @@
         <div class="col-xs-12">
             &copy; <?php echo date('Y'); ?> <a href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title(); ?></a>.
             <?php _e('由 <a href="http://www.typecho.org" target="_blank">Typecho</a> 强力驱动'); ?>.
-            Themed by <a href="http://zhangyanzi.sinaapp.com" target="_blank">史林林爱张燕子 :P</a>
+            Themed by <a href="https://github.com/shrekuu/typecho-bootstrap-theme" target="_blank">shrekuu</a>
         </div>
     </div>
 </footer><!-- end #footer -->
